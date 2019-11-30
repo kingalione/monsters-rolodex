0afbed54711f698ce48e6266103979127109be16
Ali Karadag <akrdg1988@icloud.com>
2019-11-30 22:34:46 +0100

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      title: 'Hello, Ali'
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.title}</p>
-          <button onClick={() => this.setState({ title: 'Hello, Tuba' })}>
-            Change Text
-          </button>
-        </header>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
