a4fb0729f3882ebae210aefdad6dfd4392740fec
Ali Karadag <akrdg1988@icloud.com>
2019-12-01 17:48:29 +0100

    Implement SearchField

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
 
     this.state = {
+      searchField: '',
       monsters: []
     };
   }
@@ -18,9 +19,20 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
