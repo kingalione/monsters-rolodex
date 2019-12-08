16a59ec76e5e37c54b13242bd93e4cfdc37666aa
Ali Karadag <akrdg1988@icloud.com>
2019-12-08 22:22:55 +0100

    Updates

---
+++ precache-manifest.6f21210d7f418e9f7bc7bd56c59ff463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1940b393c4ae308c7816ea4aa9153e5e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "21550a1c32a9f5f39901",
+    "url": "/monsters-rolodex/static/css/main.641af633.chunk.css"
+  },
+  {
+    "revision": "447e7e09a0cf1025e12d",
+    "url": "/monsters-rolodex/static/js/2.28889446.chunk.js"
+  },
+  {
+    "revision": "21550a1c32a9f5f39901",
+    "url": "/monsters-rolodex/static/js/main.f582835f.chunk.js"
+  },
+  {
+    "revision": "03497ef441aebc7cc47b",
+    "url": "/monsters-rolodex/static/js/runtime-main.666613d3.js"
+  }
+]);
